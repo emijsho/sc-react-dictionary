6d4d83f8ea781aee87c5a96e2933ab215da28dac
emijsho <145356364+emijsho@users.noreply.github.com>
2024-03-31 22:24:40 +0100

    amended Meaning.js to avoid map error

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,17 +5,10 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-            </p>
-          </div>
-        );
-      })}
+      <div>
+        <p>{props.meaning.definition}</p>
+        <em>{props.meaning.example}</em>
+      </div>
     </div>
   );
 }
